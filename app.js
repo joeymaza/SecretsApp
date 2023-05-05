6f67da8060d0a26d4b5c7bfce98854cc077e4f94
joeymaza <joeman67@gmail.com>
2023-05-05 09:23:29 -0400

    Hashing password

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
-const encrypt = require("mongoose-encryption");
+const md5 = require ("md5");
 
 const app = express();
 
@@ -24,7 +24,7 @@ const userSchema = new mongoose.Schema({
 });
 
 
-userSchema.plugin(encrypt,{secret:process.env.SECRET, encryptedFields:["password"]});
+
 // encrypt specific field "password", and not the entire doc
 
 const User = new mongoose.model("User", userSchema);
@@ -46,7 +46,8 @@ app.get("/register", function(req,res){
 app.post("/register", function(req,res){
     const newUser = new User({
         email:req.body.username,
-        password:req.body.password
+        password:md5(req.body.password)
+        //using hash function to encrypt password
     });
 
     newUser.save()
@@ -60,7 +61,7 @@ app.post("/register", function(req,res){
 //user register email and password to begin access to secrets page
 app.post("/login", function(req,res){
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({email:username})
     .then(function(foundUser){
